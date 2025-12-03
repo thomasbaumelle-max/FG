5b0572eeaac97f0edc8eed4186138c5ff8df6c9f
thomasbaumelle-max <thomasbaumelle@gmail.com>
2025-12-04 00:24:06 +0100

    Add biological tick simulation module

--- mapgen/__init__.py
+++ mapgen/__init__.py
@@ -1,5 +1,13 @@
 """Map generation utilities for Fantaisie."""
 
 from .continents import GeneratedMap, NodalTileData, generate_continent_map
+from .biology import BioticProfile, BiologySimulator, SpeciesType
 
-__all__ = ["generate_continent_map", "GeneratedMap", "NodalTileData"]
+__all__ = [
+    "generate_continent_map",
+    "GeneratedMap",
+    "NodalTileData",
+    "BioticProfile",
+    "BiologySimulator",
+    "SpeciesType",
+]
